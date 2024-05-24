1930c082d8aa7a1a046cad96bbcea261cb280448
shining.chen <uttw010@group.credem.net>
2024-05-24 15:42:40 +0200

    Ensured semantic HTML elements & id's for all except skeletons.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -25,11 +25,12 @@ export default function Search({ placeholder }: { placeholder: string }) {
   }, 300);
 
   return (
-    <div className="relative flex flex-1 flex-shrink-0">
+    <section id="search" className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+        id="input_keywords"
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => {
@@ -38,6 +39,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
         defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-    </div>
+    </section>
   );
 }
